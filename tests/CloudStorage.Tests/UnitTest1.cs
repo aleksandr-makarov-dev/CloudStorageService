84238e9e51410eccbc401d85a900406e61914113
Aleksandr <alexandr.makarov.2000@gmail.com>
2026-08-29 02:08:48 +0300

    feat: init project

---
+++ tests/CloudStorage.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace CloudStorage.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
