91166ab41bfdc2a6565b56c92e40c77bde0e8a9c
Aleksandr <alexandr.makarov.2000@gmail.com>
2026-09-06 02:57:37 +0300

    fix: replace parentId with id

--- src/CloudStorage.Application/Resources/CreateUploadUrl/CreateUploadUrlHandler.cs
+++ src/CloudStorage.Application/Resources/CreateUploadUrl/CreateUploadUrlHandler.cs
@@ -19,7 +19,7 @@ internal sealed class CreateUploadUrlHandler(
     {
         if (command.ParentId.HasValue &&
             !await dbContext.Resources.AnyAsync(x =>
-                x.ParentId == command.ParentId && x.IsFolder, cancellationToken))
+                x.Id == command.ParentId && x.IsFolder, cancellationToken))
         {
             logger.LogWarning("Parent folder not found. ParentFolderId: {ParentFolderId}", command.ParentId.Value);
 
